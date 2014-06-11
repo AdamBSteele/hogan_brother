0c7280fc785f021be864b92311f1333553c1445f
lurchh <as09am@my.fsu.edu>
2014-06-11 15:17:22 -0700

    Redid logic for better coverage.  Removed mentions for less annoyance

--- megaphone.py
+++ megaphone.py
@@ -6,10 +6,13 @@ import re
 
 now = datetime.datetime.now()
 
-CONSUMER_KEY = os.environ['CONSKEY']
-CONSUMER_SECRET = os.environ['CONSSECRET']
-OAUTH_TOKEN = os.environ['OTOKEN']
-OAUTH_SECRET = os.environ['OSECRET']
+Odict = {}
+# Get Oauth info from file
+lines = [line.strip() for line in open('keyfile.txt')]
+for line in lines:
+	key, value = tuple(line.split('='))
+	Odict[key] = value
+
 
 
 logging.basicConfig(filename='/root/parrot/parrotBIN/logs/megaphone.log',
@@ -18,59 +21,26 @@ logging.basicConfig(filename='/root/parrot/parrotBIN/logs/megaphone.log',
 
 # Initialize twitter
 t = Twitter(
-            auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
-                       CONSUMER_KEY, CONSUMER_SECRET)
+            auth=OAuth(Odict['OTOKEN'], Odict['OSECRET'],
+                       Odict['CONSKEY'], Odict['CONSSECRET'])
            )
+startTime = datetime.datetime.now()
 
-parrotText = t.statuses.user_timeline(screen_name="megaphonekoth")[0].get('text')
+sourceStatuses =  t.statuses.user_timeline(screen_name="jadekoth")
 
-sourceStatus =  t.statuses.user_timeline(screen_name="jadekoth")[0]
-sourceText = sourceStatus.get('text')
+tweetCount = 0
+for status in reversed(sourceStatuses[0:5]):
 
-strTime = ' '.join(sourceStatus.get('created_at').split(' ')[:4]) + ' ' + str(now.year)
-dtime = datetime.datetime.strptime(strTime, '%a %b %d %H:%M:%S %Y')
-time_diff = now - dtime
-
-if time_diff.seconds // 60 > 5:
-	logging.info("timediff " + str(time_diff.seconds // 60))
-	print "timediff " + str(time_diff.seconds // 60)
-	exit(0)
-
-logging.info("Tweeting " + sourceText)
-print "Tweeting" + sourceText
-t.statuses.update(status=sourceText)
-
-count = 0
-statusCount = 0
-
-while time_diff.seconds // 60 < 5:
-
-	logging.info("Listening for flurry. [" +
-				 str(count) + " min, " + str(statusCount) + " statuses]" ) 	
-	sleep(60)
-
-	parrotText = t.statuses.user_timeline(screen_name="megaphonekoth")[0].get('text')
-
-	sourceStatus =  t.statuses.user_timeline(screen_name="jadekoth")[0]
-	sourceText = sourceStatus.get('text')
-	
-	r = re.compile(r"(http://[^ ]+)")
-
-	if r.sub(r'', sourceText) != r.sub(r'', parrotText) and "vine" not in sourceText:
-		logging.info("Tweeting " + sourceText)
-		print "Tweeting" + sourceText
-		t.statuses.update(status=sourceText)
-		statusCount = statusCount + 1
-
-	if statusCount > 6:
-		logging.info("Counted over 6")
-		break
-
-	count = count + 1
-
-	strTime = ' '.join(sourceStatus.get('created_at').split(' ')[:4]) + ' ' + str(now.year)
+	strTime = ' '.join(status.get('created_at').split(' ')[:4]) + ' ' + str(startTime.year)
 	dtime = datetime.datetime.strptime(strTime, '%a %b %d %H:%M:%S %Y')
-	now = datetime.datetime.now()
-	time_diff = now - dtime
+	time_diff = startTime - dtime
+ 
+	if time_diff.seconds < 180:
+		sText = status.get('text')
+		newTweet = re.sub('@', '', sText)
+		logging.info("Tweeting: " + newTweet)
+		t.statuses.update(status=newTweet)
+		tweetCount += 1
+		sleep(14)
 
-print "Done listening"
+logging.info("Tweeted %d times" % tweetCount)
